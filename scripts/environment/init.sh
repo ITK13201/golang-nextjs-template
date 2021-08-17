dba110048eafad734cf4522e21ec143f3f415bf2
ITK13201 <tkik13201@gmail.com>
2021-08-18 03:51:36 +0900

    fix: mysql setting -> [query dump to logfile]

--- scripts/environment/init.sh
+++ scripts/environment/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 
+PROJECT_DIR=../..
+
 cd `dirname $0`
 
-cp -v ../../dev.env.example ../../dev.env
+cp -v ${PROJECT_DIR}/dev.env.example ${PROJECT_DIR}/dev.env
+mkdir -p ${PROJECT_DIR}/log/mysql
+touch ${PROJECT_DIR}/log/mysql/mysqld.log
+chmod 666 ${PROJECT_DIR}/log/mysql/mysqld.log
