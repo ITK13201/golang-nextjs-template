1f48ab3ef7118c68fae8de654b94ffb2357bb963
ITK13201 <tkik13201@gmail.com>
2021-08-19 19:42:19 +0900

    add: nginx log settings

--- scripts/environment/init.sh
+++ scripts/environment/init.sh
@@ -4,7 +4,17 @@ PROJECT_DIR=../..
 
 cd `dirname $0`
 
+# init environment variables
 cp -v ${PROJECT_DIR}/dev.env.example ${PROJECT_DIR}/dev.env
+
+# init mysql log files
 mkdir -p ${PROJECT_DIR}/log/mysql
 touch ${PROJECT_DIR}/log/mysql/mysqld.log
-chmod 666 ${PROJECT_DIR}/log/mysql/mysqld.log
+
+# init nginx log files
+mkdir -p -p ${PROJECT_DIR}/log/nginx
+touch ${PROJECT_DIR}/log/nginx/access.log
+touch ${PROJECT_DIR}/log/nginx/error.log
+
+# init log file permission
+find ${PROJECT_DIR}/log -type f -print | xargs chmod 666
